63b72ea99f228f52950434c173fe035d69bc0288
= <=>
2021-06-11 21:44:18 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Conversor from './componentes/Conversor'
 function App() {
   return (
     <div className="App">
-      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <h1>Money converter <span className="money">(USD to BRL)</span></h1>
+      <div className="wrap-box">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      </div>
     </div>
   );
 } 
--- src/componentes/Conversor.jsx
+++ src/componentes/Conversor.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import '../componentes/Conversor.css'
+import brasil from '../Assets/brasil.png'
+import usa from '../Assets/estados-unidos.png'
 
 export default class Conversor extends Component {
 
@@ -9,36 +12,39 @@ export default class Conversor extends Component {
             moedaA_valor:"",
             moedaB_valor: 0,
         }
-
         this.converter = this.converter.bind(this);
-
     }
 
     converter() {
-        let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
-        let url = `https://economia.awesomeapi.com.br/json/daily/USD-BRL/1`
+        let de_para = `${this.props.moedaA}-${this.props.moedaB}`;
+        let url = `https://economia.awesomeapi.com.br/json/daily/${de_para}/1`
 
         fetch(url)
         .then(res =>{
-
             return res.json()
-
         })
         .then(json=>{
             let cotacao = json[0].high;
-            let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).fixed(2)
+            let moedaB_valor = ((parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2).replace('.', ','))
             this.setState({moedaB_valor})
+
+            console.log(json[0])
         })
     }
 
     render() {
         return (
             <div className="conversor">
-                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-                <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}/> 
-                <button type="button" onClick={this.converter}>Converter</button>
-
-                <h2>{this.state.moedaB_valor}</h2>
+                <div className="wrap-dolar">
+                    <h2><span className="icone-usa"><img src={usa} width="30px"/></span>{this.props.moedaA}</h2>
+                    <input className="inputNumber" type="number" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}/> 
+                </div>
+                <button className="button" type="button" onClick={this.converter}>Converter</button>
+                <div className="wrap-real">
+                    <h2><span className="icone-brasil"><img src={brasil} width="30px"/></span>{this.props.moedaB}</h2>
+                    <div className="resultado-real" >R$ {this.state.moedaB_valor}</div>
+                </div>
+                
             </div>
         )
     }
